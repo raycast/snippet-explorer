c55caf9238e4c2b10efc07ff146e87c75f545085
Pedro Duarte <oi@ped.ro>
2023-01-25 15:00:54 +0100

    Update [[...slug]].tsx

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -139,7 +139,12 @@ export default function Home({ onTouchReady }) {
     });
 
     addedSnippets.forEach((snippet) => {
-      setSelectedSnippets((prevSnippets) => [...prevSnippets, snippet]);
+      setSelectedSnippets((prevSnippets) => {
+        if (prevSnippets.find((s) => s.id === snippet.id)) {
+          return prevSnippets;
+        }
+        return [...prevSnippets, snippet];
+      });
     });
 
     const removedSnippets = removedIds.map((id) => {
