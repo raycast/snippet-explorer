91e1588aec6b357e354b220fc3fc504349afda39
Pedro Duarte <oi@ped.ro>
2023-01-25 09:26:06 +0100

    Add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
@@ -46,6 +47,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <ToastViewport />
       </ToastProvider>
+      <Analytics />
     </>
   );
 }
