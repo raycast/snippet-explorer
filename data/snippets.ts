7be23843e38d6f1c43f833e47071760f230f03bc
Douglas Pires <douglas.pires@live.com>
2023-01-25 23:20:34 -0500

    feat: feedback-lkdn

--- data/snippets.ts
+++ data/snippets.ts
@@ -498,6 +498,15 @@ Thanks for taking the time to give us your feedback.
     keyword: "feedback-resolved",
     type: "template",
   },
+  {
+    name: "LinkedIn Feedback",
+    id: nanoid(),
+    text: `Hey {recruiter_name},
+
+I'm thrilled about the opportunity. Unfortunately, I'm currently not available to accept new offers. However, I hope we can stay connected for future positions if you don't mind. Thank you again and hope to talk to you soon.`,
+    keyword: "feedback-lkdn",
+    type: "template",
+  },
 ];
 
 const coding = [
