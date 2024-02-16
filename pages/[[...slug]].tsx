b155d7f52ed369140381e25389eaf4635e94fcf9
Pedro Duarte <oi@ped.ro>
2024-02-16 12:47:40 +0100

    Add `ref` to URL Shortener API

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -202,7 +202,7 @@ export default function Home({ onTouchReady }: { onTouchReady: () => void }) {
     const url = `${window.location.origin}/shared?${makeQueryString()}`;
     const encodedUrl = encodeURIComponent(url);
     const response = await fetch(
-      `https://ray.so/api/shorten-url?url=${encodedUrl}`
+      `https://ray.so/api/shorten-url?url=${encodedUrl}&ref=snippets`
     ).then((res) => res.json());
 
     if (response.error) {
